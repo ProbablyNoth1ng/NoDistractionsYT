22a77a65372097f0b7fbf28405decafb5a55e1a1
MRnektodotpng <android.kaplan@gmail.com>
2025-01-31 23:43:03 +0100

    shorts_update_01

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import './Main.scss'
 import Switch from '@mui/material/Switch';
 import FormControlLabel from '@mui/material/FormControlLabel';
@@ -6,45 +6,79 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 export function Main(){
     const [checked, setChecked] = useState<boolean>(false);
 
+    useEffect(() => {
+      chrome.storage?.local?.get('disableShort', (data) => {
+        setChecked(data.disableShort || false)
+        if(data.disableShort){
+          disableShort();
+        }
+      })
+    },[])   
 
     const handleChange = (event:React.ChangeEvent<HTMLInputElement>) => {
-      setChecked(event.target.checked);
-      if (event.target.checked) {
-        removeShortsMain();
+      const isChecked = event.target.checked
+      
+      setChecked(isChecked);
+      chrome.storage.local.set({disableShort: isChecked}, () => {
+        console.log("switch state saved:", isChecked)
+      })
+      if (isChecked) {
+        disableShort();
+        // console.log(chrome.storage.local.get('disableShort', data => {console.log(data)}))
       }
       
     };
 
 
-    const removeShortsMain = async () => {
+    const disableShort = async () => {
         if(chrome?.tabs){
             let [tab] = await chrome.tabs.query({active:true,currentWindow: true})
             chrome.scripting.executeScript<string[], void>({
               target: {tabId:tab.id!},
               func: () => {
-                let shorts = document.querySelectorAll('ytd-rich-grid-renderer [elements-per-row="4"]');
+                let shorts = document.querySelector('ytd-rich-grid-renderer [is-show-less-hidden]')?.closest("div #content");
+                
                 console.log(shorts)
-                shorts.forEach((elem) => {
-                    elem.remove();
-                })
+                shorts?.remove();
+                document.querySelector("ytd-reel-shelf-renderer")?.remove()
+
+
               }
             })
         }
-        
+
       }
     return (
         <>
             <main className='main'>
-                <div className="main_wrap">
+                <div className="main_wrap ">
                 <FormControlLabel
+                        className='my-1'
                         control={
                             <Switch  checked={checked} onChange={handleChange} name="" />
                         }
                         label="disable shorts on main screen"
-                        
-                        />
+                />
+                {/* <FormControlLabel
+                        className='my-1'
+                        control={
+                            <Switch  checked={checked} onChange={handleChange} name="" />
+                        }
+                        label="disable shorts on main screen"      
+                />
+                <FormControlLabel
+                        className='my-1'
+                        control={
+                            <Switch  checked={checked} onChange={handleChange} name="" />
+                        }
+                        label="disable shorts on main screen"      
+                /> */}
+                
+
+
+
                 </div>
-                       
+ 
             </main>
         </>
 
